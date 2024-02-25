969ebd72a7b9bd1442936da29aedf7c5ccafe830
Adena <adenaeisenberg@gmail.com>
2024-02-25 18:04:37 -0500

    adds logo to header

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,5 +1,11 @@
 // import { Link } from "react-router-dom";
 
 export function Header() {
-  return <h2> Header </h2>;
+  return (
+    <div>
+      <a href="/home">
+        <img src="/src/assets/Lost-&-Found-Logo.png" alt="Lost & Found Logo" width="200" />
+      </a>
+    </div>
+  );
 }
--- src/Home.jsx
+++ src/Home.jsx
@@ -1,35 +1,127 @@
 // import { Link } from "react-router-dom";
 import "./Home.css";
 
+// CODE IS FROM https://codepen.io/designfenix/pen/jOLYpNK
+
 export function Home() {
   return (
     <>
-      <div className="wrapper-images">
+      {/* <div className="wrapper-images">
         <div className="images-line">
-          <div className="line large"></div>
-          <div className="line"></div>
-          <div className="line"></div>
-          <div className="line large"></div>
-          <div className="line"></div>
-          <div className="line"></div>
+          <div
+            className="line large"
+            style={{
+              backgroundImage: `url:(https://images.unsplash.com/photo-1543466835-00a7907e9de1?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1074&q=80)`,
+            }}
+          ></div>
+          <div
+            className="line"
+            style={{
+              backgroundImage: `url:(https://images.unsplash.com/photo-1543466835-00a7907e9de1?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1074&q=80)`,
+            }}
+          ></div>
+          <div
+            className="line"
+            style={{
+              backgroundImage: `url:(https://images.unsplash.com/photo-1543466835-00a7907e9de1?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1074&q=80)`,
+            }}
+          ></div>
+          <div
+            className="line large"
+            style={{
+              backgroundImage: `url:(https://images.unsplash.com/photo-1543466835-00a7907e9de1?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1074&q=80)`,
+            }}
+          ></div>
+          <div
+            className="line"
+            style={{
+              backgroundImage: `url:(https://images.unsplash.com/photo-1543466835-00a7907e9de1?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1074&q=80)`,
+            }}
+          ></div>
+          <div
+            className="line"
+            style={{
+              backgroundImage: `url:(https://images.unsplash.com/photo-1543466835-00a7907e9de1?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1074&q=80)`,
+            }}
+          ></div>
         </div>
         <div className="images-line">
-          <div className="line"></div>
-          <div className="line large"></div>
-          <div className="line"></div>
-          <div className="line"></div>
-          <div className="line large"></div>
-          <div className="line"></div>
+          <div
+            className="line"
+            style={{
+              backgroundImage: `url:(https://images.unsplash.com/photo-1543466835-00a7907e9de1?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1074&q=80)`,
+            }}
+          ></div>
+          <div
+            className="line large"
+            style={{
+              backgroundImage: `url:(https://images.unsplash.com/photo-1543466835-00a7907e9de1?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1074&q=80)`,
+            }}
+          ></div>
+          <div
+            className="line"
+            style={{
+              backgroundImage: `url:(https://images.unsplash.com/photo-1543466835-00a7907e9de1?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1074&q=80)`,
+            }}
+          ></div>
+          <div
+            className="line"
+            style={{
+              backgroundImage: `url:(https://images.unsplash.com/photo-1543466835-00a7907e9de1?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1074&q=80)`,
+            }}
+          ></div>
+          <div
+            className="line large"
+            style={{
+              backgroundImage: `url:(https://images.unsplash.com/photo-1543466835-00a7907e9de1?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1074&q=80)`,
+            }}
+          ></div>
+          <div
+            className="line"
+            style={{
+              backgroundImage: `url:(https://images.unsplash.com/photo-1543466835-00a7907e9de1?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1074&q=80)`,
+            }}
+          ></div>
         </div>
         <div className="images-line">
-          <div className="line "></div>
-          <div className="line"></div>
-          <div className="line large"></div>
-          <div className="line "></div>
-          <div className="line"></div>
-          <div className="line large"></div>
+          <div
+            className="line "
+            style={{
+              backgroundImage: `url:(https://images.unsplash.com/photo-1543466835-00a7907e9de1?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1074&q=80)`,
+            }}
+          ></div>
+          <div
+            className="line"
+            style={{
+              backgroundImage: `url:(https://images.unsplash.com/photo-1543466835-00a7907e9de1?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1074&q=80)`,
+            }}
+          ></div>
+          <div
+            className="line large"
+            style={{
+              backgroundImage: `url:(https://images.unsplash.com/photo-1543466835-00a7907e9de1?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1074&q=80)`,
+            }}
+          ></div>
+          <div
+            className="line "
+            style={{
+              backgroundImage: `url:(https://images.unsplash.com/photo-1543466835-00a7907e9de1?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1074&q=80)`,
+            }}
+          ></div>
+          <div
+            className="line"
+            style={{
+              backgroundImage: `url:(https://images.unsplash.com/photo-1543466835-00a7907e9de1?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1074&q=80)`,
+            }}
+          ></div>
+          <div
+            className="line large"
+            style={{
+              backgroundImage: `url:(https://images.unsplash.com/photo-1543466835-00a7907e9de1?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1074&q=80)`,
+            }}
+          ></div>
         </div>
-      </div>
+      </div> */}
       <div className="container">
         <div className="logo">
           <img src="/src/assets/Lost-&-Found-Logo.png" alt="Lost & Found Logo" />
